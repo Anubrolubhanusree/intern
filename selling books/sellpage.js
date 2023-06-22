4235f263447a4519886ac09287a186a55d256071
harshita__ <mayasamaya804@gmail.com>
2023-06-22 18:32:57 +0530

    first commit

---
+++ selling books/sellpage.js
@@ -0,0 +1,8 @@
+const input = document.getElementsByClassName('btn-secondary');
+input.setAttribute('required', '');
+
+const img1 = document.getElementById('img');
+img1.setAttribute('required', '');
+
+const req1 = document.getElementById('img');
+req1.setAttribute('required', '');
